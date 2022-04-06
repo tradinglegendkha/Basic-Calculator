ce0f1cd0affbb7c1bd58920bbe4ff83086ca835d
tradinglegendkha <98289922+tradinglegendkha@users.noreply.github.com>
2022-04-06 16:33:26 -0400

    Update v1calculator.py
    update to 1.2

--- v1calculator.py
+++ v1calculator.py
@@ -1,35 +1,51 @@
-#calculator
+#calculator v1.2
 
 #name of the user
-print("Enter your name")
-name = input()
-print("Hello " + name, "this is a calculator")
+name = input("Enter your name: ")
+print("Hello "+name+ " this is a calculator")
+
+def TwoNum():
+	global n1, n2
+	print("\nPlease pick two numbers")
+	n1 = int(input("First number: "))
+	n2 = int(input("Second number: "))
 
 #numbers the user wants to calculate 
-print("\nPlease pick two numbers")
-n1 = int(input("First number "))
-n2 = int(input("Second number "))
+def fourSymbols():
+	#calculations
+	print("\nWhat do you wanna calculate? Add, Minus, Times, Divide")
+	sym = input()
 
-#calculations
-print("\nNow what do you wanna calculate? Add, Minus, Times, Divide")
-sym = input()
-if sym == "add": 
-	add = n1+n2
-	print("Answer:", n1, "+", n2, "=", add)
+	if sym == "add" or sym == "Add": 
+		TwoNum()
+		add = n1+n2
+		print("Answer:", n1, "+", n2, "=", add)
+	elif sym == "minus" or sym == "Minus":
+		TwoNum()
+		minus = n1-n2
+		print("Answer:", n1, "-", n2, "=", minus)
+	elif sym == "times" or sym == "Times":
+		TwoNum()
+		times = n1*n2
+		print("Answer:", n1, "*", n2, "=", times)
+	elif sym == "divide" or sym == "Divide":
+		TwoNum()
+		divide = n1/n2
+		print("Answer:", n1, "/", n2, "=", divide)
+	else: 
+		print("Please pick a symbol and try again " + name)
 
-elif sym == "minus":
-	minus = n1-n2
-	print("Answer:", n1, "-", n2, "=", minus)
+fourSymbols()
 
-elif sym == "times":
-	times = n1*n2
-	print("Answer:", n1, "*", n2, "=", times)
+while True:
+	#used a while loop to ask the the person
+	YorN = input("Do you want to continue? ")
+	if YorN == "yes":
+		fourSymbols()
+	else:
+		print("Thank you {}, bye".format(name))
+		break 
+#remove do you want to continue, should be seamesly
 
-elif sym == "divide":
-	divide = n1/n2
-	print("Answer:", n1, "/", n2, "=", divide)
-
-else: 
-	print("Please pick a symbol and try again " + name)
 
 
