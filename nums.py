c72fb8b9c5ec97cb62fd804a5a67e1791aff3b4d
tradinglegendkha <98289922+tradinglegendkha@users.noreply.github.com>
2022-04-06 20:13:12 -0400

    Create nums.py

---
+++ nums.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from window import entry
+
+def one(x):
+    entry.insert(INSERT, x)
+
+def two(x):
+    entry.insert(INSERT, x)
+
+def three(x):
+    entry.insert(INSERT, x)
+
+def four(x):
+    entry.insert(INSERT, x)
+
+def five(x):
+    entry.insert(INSERT, x)
+
+def six(x):
+    entry.insert(INSERT, x)
+
+def seven(x):
+    entry.insert(INSERT, x)
+
+def eight(x):
+    entry.insert(INSERT, x)
+
+def nine(x):
+    entry.insert(INSERT, x)
